bcaaa656b7294e20ab4f0af017fe0b1c7be61654
s1100h <scarincihollenbeckweb@gmail.com>
2020-02-25 14:18:56 -0500

    bug: didn't added loadable component to new components

--- client/src/pages/FrontPage/ColumnContent/index.js
+++ client/src/pages/FrontPage/ColumnContent/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import loadable from '@loadable/component';
 import PropTypes from 'prop-types';
 
 const About = loadable(() => import('./About'));
--- client/src/pages/FrontPage/Header/index.js
+++ client/src/pages/FrontPage/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import loadable from '@loadable/component';
 import cityBckGroundImg from '../../../images/skyline-background.jpg';
 
 const Content = loadable(() => import('./Content'));
