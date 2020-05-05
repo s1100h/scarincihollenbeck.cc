e817e979a96f795cede94a035a0bf79659502627
s1100h <scarincihollenbeckweb@gmail.com>
2020-05-05 12:45:47 -0400

    using nginx proxy now

--- admin/app/wp-content/plugins/shlaw-just-in-carousel/client/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import JustInCarousel from './components'
-
-ReactDOM.render(<JustInCarousel />, document.getElementById('just-in-root'))
--- admin/app/wp-content/plugins/shlaw-just-in-carousel/includes/just-in-shortcode.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if( !defined('ABSPATH')) { exit; }
-
-function justin_news_shortcode() {
-    ?><div id="just-in-root"></div><?php 
-}
