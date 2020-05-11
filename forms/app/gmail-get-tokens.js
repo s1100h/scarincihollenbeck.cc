5683b8430a932f424d2ddf2d3a8db492b0725912
s1100h <scarincihollenbeckweb@gmail.com>
2020-05-11 16:28:40 -0400

    added gmail auth to envs

--- forms/app/gmail-get-tokens.js
+++ forms/app/gmail-get-tokens.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
-  const { google } = require('googleapis');
+const { google } = require('googleapis');
 
-  // Replace with the code you've got on
-  const code = process.env.GMAIL_CODE;
- 
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.GMAIL_OAUTH_CLIENT_ID,
-    process.env.GMAIL_OAUTH_CLIENT_SECRET,
-    process.env.GMAIL_OAUTH_REDIRECT_URL,
-  );
- 
-  const getToken = async () => {
-    const { tokens } = await oauth2Client.getToken(code);
-    console.info(tokens);
-  };
- 
-  getToken();
+// Replace with the code you've got on
+const code = process.env.GMAIL_CODE;
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GMAIL_OAUTH_CLIENT_ID,
+  process.env.GMAIL_OAUTH_CLIENT_SECRET,
+  process.env.GMAIL_OAUTH_REDIRECT_URL
+);
+
+const getToken = async () => {
+  const { tokens } = await oauth2Client.getToken(code);
+  console.info(tokens);
+};
+
+getToken();
