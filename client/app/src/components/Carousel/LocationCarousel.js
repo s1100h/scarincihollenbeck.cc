d8877f7774cfafd75c52ed5ce7cc781ba0bf56e0
s1100h <scarincihollenbeckweb@gmail.com>
2020-05-28 22:32:34 -0400

    switched to a tags when linking to location pages

--- client/app/src/components/Carousel/LocationCarousel.js
+++ client/app/src/components/Carousel/LocationCarousel.js
@@ -1,6 +1,5 @@
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import LazyLoad from 'react-lazyload';
 
@@ -10,14 +9,14 @@ function LocationCarousel(props) {
 
   return (
     <div className="location-card mx-auto d-block border">
-      <Link to={slug}>
+      <a href={slug}>
         <LazyLoad height={150}>
           <img rel="preconnect" src={featuredImg} alt={title} className="mw-100 mx-auto d-block" />
         </LazyLoad>
         <p className="red-title m-3 text-uppercase">
           <strong>{title}</strong>
         </p>
-      </Link>
+      </a>
     </div>
   );
 }
