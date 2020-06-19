d897b3cda8a696532e06c0bacd7e15ffa6f1ee90
s1100h <scarincihollenbeckweb@gmail.com>
2020-06-18 21:38:58 -0400

    fixed the font issue

--- client2/app/next.config.js
+++ client2/app/next.config.js
@@ -1,9 +1,21 @@
+const withCss = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
-const withFonts = require('nextjs-fonts');
-const withCSS = require('@zeit/next-css');
+const withFonts = require('next-fonts');
 
-module.exports = withCSS(withSass(withFonts({
-  webpack(config, options) {
-   return config;
-  },
-})));
+module.exports = withFonts(withCss(
+  withSass({
+    webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+          },
+        },
+      });
+
+      return config;
+    },
+  }),
+));
--- client2/app/todos.md
+++ client2/app/todos.md
@@ -1,12 +1,13 @@
 ## Pre Production
-1. Make sure all pages & header look good
-2. Make sure the site is compatible with IE11
-4. Double check all the meta sections are correction
-5. Double check mobile & make a custom dropdown mobile menu
+
+1. Make sure the site is compatible with IE11
+2. Double check all the meta sections are correction
+3. Double check mobile & make a custom dropdown mobile menu
 
 ## Post Production
 1. Make sure captcha is working properly
 2. Make sure all forms work
+3. Fix people's pictures
 
 
 ## Post Post Production
