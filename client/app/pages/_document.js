3a51f9e6062c91069a808994a9cbfafa36dcb3fa
s1100h <scarincihollenbeckweb@gmail.com>
2020-06-22 18:34:49 -0400

    added GA and search console stuff

--- client/app/pages/_document.js
+++ client/app/pages/_document.js
@@ -35,14 +35,23 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8"/>
           <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport"/>
           <meta name="msvalidate.01" content="D568BE2730F6C27E33061E84F8DE58B1" />
-          <meta name="google-site-verification" content="googlee1788c62f584220b" /> 
+          <meta name="google-site-verification" content="ulImKkFX6Wqx04n63QzoiKMPDPwbQ53bDZAfwxyMq2Q" />
         </Head>
         <body>
           <Main />
           <NextScript />
           <script
             dangerouslySetInnerHTML={{
-              __html: `[google analytics tracking code here]`
+              __html: `<!-- Global site tag (gtag.js) - Google Analytics -->
+              <script async src="https://www.googletagmanager.com/gtag/js?id=UA-18813670-1"></script>
+              <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+              
+                gtag('config', 'UA-18813670-1');
+              </script>
+              `
             }}
           />
         </body>
