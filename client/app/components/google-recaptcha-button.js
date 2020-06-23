92e84d6584cdd5fe439150cced9deba82b45faa2
s1100h <scarincihollenbeckweb@gmail.com>
2020-06-22 20:48:37 -0400

    uploading new captcha and new ignore rules

--- client/app/components/google-recaptcha-button.js
+++ client/app/components/google-recaptcha-button.js
@@ -7,5 +7,5 @@ export default function FormReCaptcha(props) {
       setCaptcha(false);
     }
   }
-  return <div className="d-block w-100"><ReCAPTCHA sitekey="6LeC96QZAAAAACJ64-6i0e-wibaQpwEpRPcnWNdY" className="mb-3" onChange={onChange} /></div>
+  return <div className="d-block w-100"><ReCAPTCHA sitekey="6Ld5QKgZAAAAAJgItlSz9xTxB1zay1u48kEOkhT1" className="mb-3" onChange={onChange} /></div>
 } 
