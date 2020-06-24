bcb2954312fbe3a3fc30aa1bc640e96b0101b8fe
s1100h <scarincihollenbeckweb@gmail.com>
2020-06-23 20:57:16 -0400

    added favicons to site head

--- client/app/pages/_document.js
+++ client/app/pages/_document.js
@@ -37,6 +37,10 @@ export default class MyDocument extends Document {
           <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport" />
           <meta name="msvalidate.01" content="D568BE2730F6C27E33061E84F8DE58B1" />
           <meta name="google-site-verification" content="ulImKkFX6Wqx04n63QzoiKMPDPwbQ53bDZAfwxyMq2Q" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon-16x16.png"/>
+          <link rel="manifest" href="/public/site.webmanifest"/>
         </Head>
         <body>
           <Main />
