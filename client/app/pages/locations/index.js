cc49b541f96b9be9ad909a1774b2ba6048e578a8
s1100h <scarincihollenbeckweb@gmail.com>
2020-07-08 15:57:41 -0400

    added getServerSideProps to locations page

--- client/app/pages/locations/index.js
+++ client/app/pages/locations/index.js
@@ -68,7 +68,7 @@ export default function Location({
 }
 
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const [locations, lyndhurst, lyndhurstposts, slides] = await Promise.all([
     fetch(`${process.env.REACT_APP_CACHED_API}/cached/office-locations`, { headers }).then((data) => data.json()),
     fetch(`${process.env.REACT_APP_WP_BACKEND}/wp-json/individual-location/office/lyndhurst`, { headers }).then((data) => data.json()),
