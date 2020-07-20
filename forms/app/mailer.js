3f776c889a183ac5c89206959ae5ed4b70e4eb81
Peter F. Tumulty <ptumulty@sh-law.com>
2020-07-20 11:46:48 -0400

    checking creds

--- forms/app/mailer.js
+++ forms/app/mailer.js
@@ -96,7 +96,15 @@ exports.inquiry = async (inq) => {
 			</div>
 		`
 	};
-	return inquiryMessage;
+	return {
+		type: 'OAuth2',
+		user: process.env.GMAIL_ADDRESS,
+		clientId: process.env.GMAIL_OAUTH_CLIENT_ID,
+		clientSecret: process.env.GMAIL_OAUTH_CLIENT_SECRET,
+		refreshToken: process.env.GMAIL_OAUTH_REFRESH_TOKEN,
+		accessToken: process.env.GMAIL_OAUTH_ACCESS_TOKEN,
+		expires: Number.parseInt(process.env.GMAIL_OAUTH_TOKEN_EXPIRE, 10),
+	}
 };
 
 
