1d51a5514aa369b54908db228c7190cc9dca3be9
ptums <ptumulty923@gmail.com>
2020-11-13 23:50:18 -0500

    removed app dir and test file

--- client/app/node_modules/react-recaptcha-google/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReCaptcha from './ReCaptcha';
-import loadReCaptcha from './loadReCaptcha';
-
-
-module.exports = {
-  ReCaptcha: ReCaptcha,
-  loadReCaptcha: loadReCaptcha
-}
