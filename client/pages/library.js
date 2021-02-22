3e8d8dc5963c92c616d598da097ba04acb9ec770
ptums <ptumulty923@gmail.com>
2021-02-22 12:34:43 -0500

    added algolia keys to env file

--- client/pages/library.js
+++ client/pages/library.js
@@ -1,7 +1,34 @@
-export default function Library() {
+import algoliasearch from 'algoliasearch';
+
+export default function Library({ results }) {
+  // get page two of results, then we need a mechanism to load more posts on click
   return (
     <>
       New powerhouse Firm Article Library will go here...
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const client = algoliasearch(process.env.NEXT_PUBLISH_ALGOLIA_APP_ID, process.env.NEXT_PUBLISH_ALGOLIA_API_KEY);
+  const index = client.initIndex('wp_searchable_posts');
+
+  // everything should be powered by algio search
+  index
+    .search('Donald Scarinci')
+    .then(({ hits }) => {
+      console.log(hits.length);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  /**
+   * Standard queries - authors
+   */
+  return {
+    props: {
+      results: [],
+    },
+  };
 }
