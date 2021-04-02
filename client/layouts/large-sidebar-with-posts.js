b1247b8b183e3524ed1bccd21ce7e2c594bd3d2f
Peter Tumulty <ptumulty923@gmail.com>
2021-04-02 11:54:29 -0400

    fully removed react-virtualized

--- client/layouts/large-sidebar-with-posts.js
+++ client/layouts/large-sidebar-with-posts.js
@@ -3,38 +3,10 @@ import Image from 'next/image';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { List } from 'react-virtualized';
 import ContactForm from 'components/contact-form';
 import { formatDate, createMarkup } from 'utils/helpers';
 import styles from 'styles/BasicContent.module.css';
 import grayTitleStyles from 'styles/BigGrayTitle.module.css';
-
-function Post(content, { key, index }) {
-  const post = content[index];
-
-  return (
-    <li key={key} className="my-3 pb-3 border-bottom">
-      <Link href={post.link}>
-        <a className="d-flex flex-row text-dark">
-          <Image
-            src={post.fimg_url || '/images/no-image-found-diamond.png'}
-            alt={post.title.rendered}
-            width={196}
-            height={98}
-            layout="intrinsic"
-            className="mr-3"
-          />
-          <div>
-            <h5 className="d-block w-100 mb-0 pt-0">
-              <strong>{post.title.rendered}</strong>
-            </h5>
-            <p className="mt-0 pt-0">{formatDate(post.date)}</p>
-          </div>
-        </a>
-      </Link>
-    </li>
-  );
-}
 
 export default function LargeSidebarWithPosts({
   content,
@@ -55,13 +27,28 @@ export default function LargeSidebarWithPosts({
           </h3>
           <hr />
           <ul className="my-0 py-0">
-            <List
-              width={800}
-              height={600}
-              rowCount={posts.length}
-              rowHeight={250}
-              rowRenderer={(rowArgs) => Post(posts, rowArgs)}
-            />
+            {posts.length > 0 && posts.map((post) => (
+              <li key={post.title.rendered} className="my-3 pb-3 border-bottom">
+              <Link href={post.link}>
+                <a className="d-flex flex-row text-dark">
+                  <Image
+                    src={post.fimg_url || '/images/no-image-found-diamond.png'}
+                    alt={post.title.rendered}
+                    width={196}
+                    height={98}
+                    layout="intrinsic"
+                    className="mr-3"
+                  />
+                  <div>
+                    <h5 className="d-block w-100 mb-0 pt-0">
+                      <strong>{post.title.rendered}</strong>
+                    </h5>
+                    <p className="mt-0 pt-0">{formatDate(post.date)}</p>
+                  </div>
+                </a>
+              </Link>
+            </li>
+            ))}
           </ul>
           <div className="mt-5 w-100">
             <h4 className={grayTitleStyles.title}>
