835f9b77b931a5791bd303680be061492b5713c4
Peter Tumulty <ptumulty923@gmail.com>
2021-04-11 01:11:16 -0400

    added animation to templates

--- client/components/home/banner.js
+++ client/components/home/banner.js
@@ -6,22 +6,24 @@ export default function HomeBanner() {
   return (
     <div className={styles.banner}>
       <div
-        className={`${styles.bannerContainer} ${styles.homeBanner} animate__animated animate__fadeInUp animate__fast`}
+        className={`${styles.homeBannerContainer} ${styles.homeBanner}`}
       >
-        <div className="d-flex border p-4">
+        <div className="d-flex p-4">
           <Image
             src="/images/scarinci-hollenbeck-diamond-badge-SVG.svg"
             alt="scarinci hollenbeck diamond logo"
             width={110}
             height={95}
-            className="mr-2 mb-3"
+            className="mr-2 mb-3 animate__animated animate__fadeInLeft animate__slow"
           />
           <div>
-            <h2 className={styles.h2Text}>Ready to rebuild?</h2>
-            <h3 className={styles.h3Text}>
+            <h2 className={`${styles.h2Text} animate__animated animate__fadeInDown animate__slow`}>Ready to rebuild?</h2>
+            <h3 className={`${styles.h3Text} animate__animated animate__fadeInDown animate__slow`}>
               <strong>We are here to help</strong>
             </h3>
-            <FrontSearch />
+            <div className="animate__animated animate__fadeInUp animate__slow">
+              <FrontSearch />
+            </div>
           </div>
         </div>
       </div>
--- client/layouts/single-sub-header.js
+++ client/layouts/single-sub-header.js
@@ -5,17 +5,17 @@ export default function SingleSubHeader({ title, subtitle, isBlog }) {
   return (
     <div className={styles.backPageBanner}>
       <div
-        className={`${styles.backBanner} animate__animated animate__fadeInUp animate__fast`}
+        className={styles.backBanner}
       >
         <div
           className="d-flex flex-column p-4"
           style={{ maxWidth: 900 }}
         >
-          <h1 className="text-white mb-0" style={{ fontSize: !isBlog ? '4rem': null, textShadow: '2px 3px 3px #000'}}>
+          <h1 className="text-white mb-0 animate__animated animate__fadeInDown animate__fast" style={{ fontSize: !isBlog ? '4rem': null, textShadow: '2px 3px 3px #000'}}>
             <strong>{title}</strong>
           </h1>
           <div
-            className="w-100 d-block my-2"
+            className="w-100 d-block my-2 animate__animated animate__fadeInDown animate__fast"
             style={{
               height: '3px',
               backgroundColor: '#db2220',
@@ -23,7 +23,7 @@ export default function SingleSubHeader({ title, subtitle, isBlog }) {
             }}
           />
           <h2
-            className="mt-2 mb-0 text-white"
+            className="mt-2 mb-0 text-white animate__animated animate__fadeInUp animate__fast"
             style={{ fontSize: '1.4rem' }}
             dangerouslySetInnerHTML={createMarkup(subtitle)}
           />
