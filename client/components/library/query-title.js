b376114d84a8348893f229e18e3b3436cc451ca4
Peter Tumulty <ptumulty923@gmail.com>
2021-04-27 09:57:37 -0400

    added bold around selected title

--- client/components/library/query-title.js
+++ client/components/library/query-title.js
@@ -2,7 +2,11 @@ const QueryTitle = ({ title }) => (
   <h4 className="mt-4 mb-0 d-block">
     Results for
     {' '}
-    <u className="text-capitalize">{title}</u>
+    <u className="text-capitalize">
+      <strong>
+        {title}
+      </strong>
+    </u>
     {' '}
     articles
     <style jsx>{' h4{ font-size: 1.25rem}'}</style>
