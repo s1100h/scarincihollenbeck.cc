32e3b0435a3614d665e54671efeef8bcbcba2086
Peter Tumulty <ptumulty923@gmail.com>
2021-06-22 08:50:54 -0400

    fixed up related practice btn

--- components/singlepractice/links.js
+++ components/singlepractice/links.js
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { useRouter } from 'next/router';
 import Nav from 'react-bootstrap/Nav';
+import ClipLoader from 'react-spinners/ClipLoader';
 import styles from 'styles/Tabs.module.css';
 import { urlify } from 'utils/helpers';
 
 export default function PracticeLinks({ links, practiceUrl }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
   const tabLinks = links.content;
   const relatedArticlesLink = urlify(practiceUrl);
 
   function handleRelatedArticlesLink(term) {
+    setLoading(true);
+
     router.push({
       pathname: '/library',
       query: { term },
@@ -36,8 +41,8 @@ export default function PracticeLinks({ links, practiceUrl }) {
           onClick={() => handleRelatedArticlesLink(relatedArticlesLink)}
           className={`${styles.tab} ${styles.practice} text-white`}
           style={{ display: 'block', padding: '.5rem 1rem', border: 0 }}
-        >
-          Related Updates
+        >          
+          {loading ? <ClipLoader loading={loading} size={12} color="#FFF" /> : <>Related Updates</>}
         </button>
       </Nav.Item>
     </Nav>
