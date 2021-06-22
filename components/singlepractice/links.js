addf2389d5aabfe59e7231a2bef42d8d3b5ffbf9
Peter Tumulty <ptumulty923@gmail.com>
2021-06-22 08:37:24 -0400

    made related practice links a button rather than a link

--- components/singlepractice/links.js
+++ components/singlepractice/links.js
@@ -1,9 +1,19 @@
+import { useRouter } from 'next/router';
 import Nav from 'react-bootstrap/Nav';
 import styles from 'styles/Tabs.module.css';
 import { urlify } from 'utils/helpers';
 
 export default function PracticeLinks({ links, practiceUrl }) {
+  const router = useRouter();
   const tabLinks = links.content;
+  const relatedArticlesLink = urlify(practiceUrl);
+
+  function handleRelatedArticlesLink(term) {
+    router.push({
+      pathname: '/library',
+      query: { term },
+    })
+  }
 
   return (
     <Nav
@@ -22,13 +32,13 @@ export default function PracticeLinks({ links, practiceUrl }) {
         </Nav.Item>
       ))}
       <Nav.Item as="li">
-        <a
-          href={`/library?term=${urlify(practiceUrl)}`}
+        <button
+          onClick={() => handleRelatedArticlesLink(relatedArticlesLink)}
           className={`${styles.tab} ${styles.practice} text-white`}
-          style={{ display: 'block', padding: '.5rem 1rem' }}
+          style={{ display: 'block', padding: '.5rem 1rem', border: 0 }}
         >
           Related Updates
-        </a>
+        </button>
       </Nav.Item>
     </Nav>
   );
