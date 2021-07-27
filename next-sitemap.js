bccc0da1b53f709b35a430dc570604256fa57275
Peter Tumulty <ptumulty923@gmail.com>
2021-07-27 08:09:03 -0400

    excluded site-forms from robot.txt and sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteUrl: 'https://scarincihollenbeck.com',
-  exclude: ['/attorney/howard-d-bader/content/awards-&-recognitions'],
+  exclude: [
+    '/attorney/howard-d-bader/content/awards-&-recognitions',
+    '/site-forms/attorney-content-updates'
+  ],
   changefreq: 'monthly',
   generateRobotsTxt: false
 }
