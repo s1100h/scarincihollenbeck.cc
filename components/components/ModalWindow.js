101f777daf4ef4043e25db5b9d243c787f2eef8f
Khyzhniak <khyzhniakihor@gmail.com>
2022-09-02 19:17:15 +0300

    change text in modal window

--- components/components/ModalWindow.js
+++ components/components/ModalWindow.js
@@ -23,9 +23,9 @@ function ModalWindow() {
         <Modal toggle={() => setModalOpen(!modalOpen)} isOpen={modalOpen}>
           <div className="modal-header modal-header_my">
             <h5 className="modal-title_my" id="exampleModalLabel">
-              Our Office is Moving!
+              Our Lyndhurst Office Moved!
             </h5>
-            <p>Effective September 6, 2022, our Lyndhurst office is moving to:</p>
+            <p>Effective September 6, 2022, our new address is:</p>
             <a
               className="modal-link"
               href="https://scarincihollenbeck.com/firm-news/scarinci-moving-to-overlook-corporate-center"
@@ -33,10 +33,9 @@ function ModalWindow() {
               150 Clove Road 9th Floor Little Falls, NJ, 07424
             </a>
             <p>
-              Please be advised that there will be no disruption of service. Scarinci Hollenbeck’s
-              contact information, such as email addresses and phone numbers, will remain the same.
-              For any questions regarding the move, contact the attorney with whom you work, or call
-              us at 201-896-4100.
+              Scarinci Hollenbecks contact information, such as email addresses and phone numbers,
+              remains the same. For any questions regarding the move, contact the attorney with whom
+              you work, or call us at 201-896-4100.
               {' '}
             </p>
             <button
