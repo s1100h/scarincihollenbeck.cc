075ff3a112447dd818c9ca757a6d62236cb8454a
Artem Kabanov <artemkabanov108@gmail.com>
2022-12-28 19:49:41 +0200

    fix: cleaning SEO part 4

--- components/organisms/post/AuthorBio.js
+++ components/organisms/post/AuthorBio.js
@@ -1,34 +1,38 @@
 import Link from 'next/link';
+import { useId } from 'react';
 import { SITE_TITLE } from 'utils/constants';
 import { createMarkup } from 'utils/helpers';
 
 const PostAuthorBio = ({ authors }) => (
   <div className="w-100 d-print-none mt-5">
-    {authors.map((a) => (
-      <div key={a.display_name} className="mb-2">
+    {authors.map(
+      (author) => author.statusProfile !== null && (
+      <div key={author.ID || useId()} className="mb-2">
         <div className=" d-print-none mt-3">
           <h3>
             About
-            <span className="ml-2">{a.display_name}</span>
+            {' '}
+            <span className="ml-2">{author.display_name}</span>
           </h3>
         </div>
         <div className="pt-4 d-flex flex-column flex-md-row">
           <div className="mt-3">
             <p>
               <span
-                dangerouslySetInnerHTML={createMarkup(a.authorDescription)}
+                dangerouslySetInnerHTML={createMarkup(author.authorDescription)}
                 className="d-block"
               />
-              {a.name !== SITE_TITLE && (
-                <Link href={a.user_url ? a.user_url : '/attorneys/'}>
-                  <a className="my-2 d-block">Full Biography</a>
-                </Link>
+              {author.name !== SITE_TITLE && (
+              <Link href={author.user_url ? `/attorneys/${author.user_url}` : '/attorneys'}>
+                <a className="my-2 d-block">Full Biography</a>
+              </Link>
               )}
             </p>
           </div>
         </div>
       </div>
-    ))}
+      ),
+    )}
   </div>
 );
 
