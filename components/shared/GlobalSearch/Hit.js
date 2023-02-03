6fd340f05efa5bb993643acce01af553b3289eda
Artem Kabanov <artemkabanov108@gmail.com>
2023-02-03 11:06:59 +0200

    feat: post search add Link

--- components/shared/GlobalSearch/Hit.js
+++ components/shared/GlobalSearch/Hit.js
@@ -1,22 +1,25 @@
 import { SearchedItem } from 'styles/Hit.style';
-import { BASE_API_URL } from '../../../utils/constants';
+import Link from 'next/link';
+import { PRODUCTION_URL } from '../../../utils/constants';
 
 export default function Hit({ hit }) {
   return (
-    <SearchedItem href={hit.permalink.replace(BASE_API_URL, '')}>
-      <p className="post-type">{hit.post_type_label}</p>
-      <p className="title">
-        <strong>{hit.post_title}</strong>
-      </p>
-      <p className="details">
-        {hit.post_type === 'post' && <>{hit.post_author.display_name}</>}
-        {hit.post_type === 'post' && (
-          <>
-            <span className="mx-1">|</span>
-            {hit.post_date_formatted}
-          </>
-        )}
-      </p>
-    </SearchedItem>
+    <Link href={hit.permalink.replace(PRODUCTION_URL, '')} passHref>
+      <SearchedItem>
+        <p className="post-type">{hit.post_type_label}</p>
+        <h4 className="title">
+          <strong>{hit.post_title}</strong>
+        </h4>
+        <p className="details">
+          {hit.post_type === 'post' && <>{hit.post_author.display_name}</>}
+          {hit.post_type === 'post' && (
+            <>
+              <span className="mx-1">|</span>
+              {hit.post_date_formatted}
+            </>
+          )}
+        </p>
+      </SearchedItem>
+    </Link>
   );
 }
