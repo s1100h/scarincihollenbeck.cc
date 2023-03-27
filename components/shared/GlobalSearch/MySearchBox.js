d242f31dc297115a4fc067764e608c19c5a54418
Artem Kabanov <artemkabanov108@gmail.com>
2023-03-27 20:03:50 +0300

    fix: close search list by click

--- components/shared/GlobalSearch/MySearchBox.js
+++ components/shared/GlobalSearch/MySearchBox.js
@@ -1,6 +1,14 @@
 import { Form } from 'react-bootstrap';
 import { BsSearch, BsXLg } from 'react-icons/bs';
-import { SearchForm, SearchInput } from 'styles/GlobalSearch.style';
+import {
+  HitsStyled,
+  ResultsContainer,
+  SearchForm,
+  SearchInput,
+  ResultBox,
+} from 'styles/GlobalSearch.style';
+import { connectStateResults, Pagination } from 'react-instantsearch-dom';
+import Hit from './Hit';
 
 function MySearchBox(props) {
   const handleClear = () => {
@@ -10,18 +18,34 @@ function MySearchBox(props) {
       },
     });
   };
+
+  const RenderSearchResults = connectStateResults(({ searchResults }) => (
+    <>
+      {searchResults?.query.length > 0 && searchResults?.hits.length > 0 && (
+        <ResultsContainer>
+          <Pagination totalPages={10} />
+          <ResultBox onClick={handleClear} className="results-container">
+            <HitsStyled hitComponent={Hit} />
+          </ResultBox>
+        </ResultsContainer>
+      )}
+    </>
+  ));
   return (
-    <SearchForm>
-      <Form.Group className="form-group" controlId="siteSearch">
-        {!props.currentRefinement ? <BsSearch /> : <BsXLg role="button" onClick={handleClear} />}
-        <SearchInput
-          type="input"
-          value={props.currentRefinement}
-          onChange={props.refine}
-          placeholder={props.placeholder}
-        />
-      </Form.Group>
-    </SearchForm>
+    <>
+      <SearchForm>
+        <Form.Group className="form-group" controlId="siteSearch">
+          {!props.currentRefinement ? <BsSearch /> : <BsXLg role="button" onClick={handleClear} />}
+          <SearchInput
+            type="input"
+            value={props.currentRefinement}
+            onChange={props.refine}
+            placeholder={props.placeholder}
+          />
+        </Form.Group>
+      </SearchForm>
+      <RenderSearchResults />
+    </>
   );
 }
 
