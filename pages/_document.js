d79d19747ac7dab974004496e4423926a894e1bf
Artem Kabanov <artemkabanov108@gmail.com>
2023-05-16 14:41:23 +0300

    feat: google tag manager was added v3

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,6 @@ import Document, {
   Html, Head, Main, NextScript,
 } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
-import { createMarkup } from '../utils/helpers';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -51,19 +50,6 @@ export default class MyDocument extends Document {
           `,
             }}
           />
-          {/* Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={createMarkup(
-              `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PC64FQH');
-          `,
-            )}
-          />
-          {/* End Google Tag Manager */}
         </Head>
         <body>
           {/* Google Tag Manager (noscript) */}
