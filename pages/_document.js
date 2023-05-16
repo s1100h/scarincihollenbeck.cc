afd25659e239f1f7828381bb2e87b27472353973
Artem Kabanov <artemkabanov108@gmail.com>
2023-05-16 14:13:01 +0300

    feat: google tag manager was added v1

--- pages/_document.js
+++ pages/_document.js
@@ -57,16 +57,11 @@ export default class MyDocument extends Document {
             async
             dangerouslySetInnerHTML={createMarkup(
               `
-              (function(w,d,s,l,i){
-                w[l] = w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l='+l : '';
-                j.async = true;
-                j.src = 'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              }
-              )(window, document, 'script', 'dataLayer', 'GTM-PC64FQH');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PC64FQH');
           `,
             )}
           />
