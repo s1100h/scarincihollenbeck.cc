0a70e79457202e1cf2db16bd0cd7b660456ce117
Pabli <pavel.vikhristyuk@fountainhead-it.com>
2023-06-16 15:37:25 +0300

    limit on phone number input, add target property for external links

--- components/shared/ContactForm/RenderInputs.js
+++ components/shared/ContactForm/RenderInputs.js
@@ -2,6 +2,17 @@ import { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import { FormLabelStyled, InputGroupStyled } from 'styles/attorney-page/GetInTouchForm.styles';
 
+const InputField = ({ onChange, ...props }) => {
+  const [value, setValue] = useState('');
+  const onInput = (event) => {
+    const value = props.name === 'Phone' ? event.target.value.replace(/[^0-9+() ]/g, '') : event.target.value;
+    setValue(value);
+    onChange(event, props.name);
+  };
+
+  return <Form.Control value={value} onChange={onInput} {...props} />;
+};
+
 const RenderInputs = ({ arrayOfAttributes, attorneySlug }) => {
   const [isNotEmptyInput, setIsNotEmpty] = useState([]);
 
@@ -13,31 +24,14 @@ const RenderInputs = ({ arrayOfAttributes, attorneySlug }) => {
 
   return (
     <>
-      <input
-        type="hidden"
-        name="currentPage"
-        value={`https://scarincihollenbeck.com${attorneySlug}`}
-      />
+      <input type="hidden" name="currentPage" value={`https://scarincihollenbeck.com${attorneySlug}`} />
       {arrayOfAttributes.map((attributes) => (
         <InputGroupStyled key={attributes.name}>
-          <FormLabelStyled visuallyHidden={!isNotEmptyInput.includes(attributes.name)}>
-            {attributes.name}
-          </FormLabelStyled>
-          <Form.Control
-            {...attributes}
-            onChange={(event) => handleChangeValue(event, attributes.name)}
-          />
+          <FormLabelStyled visuallyHidden={!isNotEmptyInput.includes(attributes.name)}>{attributes.name}</FormLabelStyled>
+          <InputField {...attributes} onChange={(event) => handleChangeValue(event, attributes.name)} />
         </InputGroupStyled>
       ))}
-      <textarea
-        type="textarea"
-        rows="8"
-        cols="4"
-        className="form-control"
-        name="Message"
-        placeholder="Message"
-        rules="required|max:1000"
-      />
+      <textarea type="textarea" rows="8" cols="4" className="form-control" name="Message" placeholder="Message" rules="required|max:1000" />
     </>
   );
 };
