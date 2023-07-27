32dd904c0a7900c3038c57a6689d2736cfcdb1b7
Alexandr Matviienko <matvienko@mettevo.org>
2023-07-27 11:52:56 +0300

    remove console.log

---
+++ components/atoms/UploadFileInput.js
@@ -0,0 +1,46 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { Form } from 'react-bootstrap';
+import { BsPaperclip } from 'react-icons/bs';
+import {
+  UploadContainer, UploadIcon, UploadRow, UploadTitle,
+} from '../../styles/UploadFileInput.style';
+
+const UploadFileInput = ({ onChange, ...attributes }) => {
+  const [fileName, setFileName] = useState('');
+  const ref = useRef(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    onChange(event, attributes.name);
+    setFileName(file.name);
+  };
+
+  const handleClick = () => {
+    ref.current.click();
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      ref.current.click();
+    }
+  };
+
+  return (
+    <UploadContainer>
+      <UploadTitle>{attributes.label}</UploadTitle>
+      <UploadRow>
+        <UploadIcon>
+          <BsPaperclip size={20} />
+        </UploadIcon>
+        <Form.Control type="file" onChange={handleFileChange} ref={ref} htmlSize={0} className="d-none" {...attributes} role="button" tabIndex="0" onKeyDown={handleKeyDown} />
+        <div className="w-100" role="button" tabIndex="0" onClick={handleClick} onKeyDown={handleKeyDown}>
+          <Form.Control type="text" name={`${attributes?.name}Text`} value={fileName} placeholder={attributes.placeholder || 'Attach the file'} readOnly disabled />
+        </div>
+      </UploadRow>
+    </UploadContainer>
+  );
+};
+
+export default UploadFileInput;
--- components/molecules/CareerForm.js
+++ components/molecules/CareerForm.js
@@ -3,118 +3,37 @@ import { useContext, useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
 import { Col, Row } from 'react-bootstrap';
 import kwesforms from 'kwesforms';
+import { FormContainer } from 'styles/attorney-page/GetInTouchForm.styles';
 import { StandardRedButton } from '../../styles/Buttons.style';
 import { FormsContext } from '../../contexts/FormsContext';
-import { THANKS_MESSAGE } from '../../utils/constants';
+import {
+  THANKS_MESSAGE, inputsCareerForm, GET_IN_TOUCH_FORM_API, RECAPTCHA_SITE_KEY,
+} from '../../utils/constants';
 import { CareerFormContainer } from '../../styles/Careers.style';
+import RenderInputs from '../shared/ContactForm/RenderInputs';
+import { TwoColumnsForm } from '../../styles/attorney-page/GetInTouchForm.styles';
+import UploadFileInput from '../atoms/UploadFileInput';
 
 const KwesScripts = dynamic(() => import('components/shared/KwesScripts'));
 
-const CareerForm = ({ contact, title }) => {
+const CareerForm = () => {
   const router = useRouter();
   const { handleCheckDisclaimer, isCheckedDisclaimer } = useContext(FormsContext);
-  const [isFileAdded, setIsFileAdded] = useState({
-    coverLetter: false,
-    resume: false,
-  });
-
-  const resumeInputName = 'resume';
-  const coverLetterInputName = 'coverLetter';
 
   useEffect(() => {
     kwesforms.init();
   }, []);
 
-  const handleAddFile = (event) => {
-    const inputName = event.target.name;
-    if (inputName === resumeInputName) {
-      setIsFileAdded((prev) => ({
-        resume: true,
-        coverLetter: prev.coverLetter,
-      }));
-    }
-    if (inputName === coverLetterInputName) {
-      setIsFileAdded((prev) => ({
-        resume: prev.resume,
-        coverLetter: true,
-      }));
-    }
-  };
-
-  const isDisabledSubmitButton = !isCheckedDisclaimer || !isFileAdded.resume || !isFileAdded.coverLetter;
+  const isDisabledSubmitButton = !isCheckedDisclaimer;
 
   return (
-    <CareerFormContainer>
+    <FormContainer isPositionRelative>
       <KwesScripts />
-      <form method="POST" action="https://kwes.io/api/foreign/forms/rKYfR2fNcm68wzPCSiyW" encType="multipart/form-data" className="kwes-form d-print-none px-1" has-recaptcha-v3="true" recaptcha-site-key="6LeC96QZAAAAACJ64-6i0e-wibaQpwEpRPcnWNdY" success-message={THANKS_MESSAGE.title}>
-        <input type="hidden" name="currentPage" value={`https://scarincihollenbeck.com${router.asPath}`} />
-        <input type="hidden" name="currentTitle" value={title} />
-        <input type="hidden" name="currentContact" value={contact} />
-        <Row className="mb-3">
-          <Col sm={12} md={6} className="mx-0 px-1">
-            <input type="text" className="form-control mx-0" name="firstName" placeholder="First name" rules="required|max:255" />
-          </Col>
-          <Col sm={12} md={6} className="mx-0 px-1">
-            <input type="text" className="form-control mx-0" name="lastName" placeholder="Last name" rules="required|max:255" />
-          </Col>
-        </Row>
-        <Row className="mb-3">
-          <Col sm={12} md={6} className="mx-0 px-1">
-            <input type="email" className="form-control mx-0" name="email" placeholder="Email address" rules="required|max:255" />
-          </Col>
-          <Col sm={12} md={6} className="mx-0 px-1">
-            <input
-              type="phone"
-              className="form-control mx-0"
-              name="phone"
-              placeholder="Phone number"
-              rules="required|regex:/^(\+?1[-.\s]?)?(\()?\d{3}(\))?[-.\s]?\d{3}[-.\s]?\d{4}$/"
-              title="Please use these formats:
-                    (123) 456-7890
-                    123-456-7890
-                    123.456.7890
-                    123 456 7890
-                    +1 123-456-7890
-                    +1 (123) 456-7890"
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={12} className="mx-0 px-1">
-            <label htmlFor="coverLetter">
-              <span className="d-flex w-100 my-2 gap-1 flex-wrap">
-                <strong>Upload your cover letter</strong>
-                <span>(this is required field)</span>
-              </span>
-              <input onChange={handleAddFile} type="file" name={coverLetterInputName} id="coverLetter" rules="required" />
-            </label>
-          </Col>
-          <Col sm={12} className="mx-0 px-1">
-            <label htmlFor="resume">
-              <span className="d-flex w-100 my-2 gap-1 flex-wrap">
-                <strong>Upload your resume</strong>
-                <span>(this is required field)</span>
-              </span>
-              <input onChange={handleAddFile} type="file" name={resumeInputName} id="resume" rules="required" />
-            </label>
-          </Col>
-          <Col sm={12} className="mx-0 px-1">
-            <label htmlFor="writing">
-              <span className="d-block w-100 my-2">
-                <strong>Upload a writing sample</strong>
-              </span>
-              <input type="file" name="writing" id="writing" />
-            </label>
-          </Col>
-          <Col sm={12} className="mx-0 px-1">
-            <label htmlFor="transcript">
-              <span className="d-block w-100 my-2">
-                <strong>Upload a transcript</strong>
-              </span>
-              <input type="file" name="transcript" id="transcript" />
-            </label>
-          </Col>
-        </Row>
+      <TwoColumnsForm action={GET_IN_TOUCH_FORM_API} className="kwes-form d-print-none w-100" has-recaptcha-v3="true" recaptcha-site-key={RECAPTCHA_SITE_KEY} success-message={THANKS_MESSAGE.title}>
+        <div className="career-form-wrapper">
+          <RenderInputs arrayOfAttributes={inputsCareerForm} attorneySlug={router.asPath} />
+        </div>
+
         <Row className="mb-0">
           <Col sm={12} className="mx-0 px-1">
             <p className="mb-1">* The use of the Internet or this form for communication with the firm or any individual member of the firm does not establish an attorney-client relationship. Confidential or time-sensitive information should not be sent through this form.</p>
@@ -129,8 +48,8 @@ const CareerForm = ({ contact, title }) => {
         <StandardRedButton disabled={isDisabledSubmitButton} className="mt-2 ml-0" type="submit">
           Submit form
         </StandardRedButton>
-      </form>
-    </CareerFormContainer>
+      </TwoColumnsForm>
+    </FormContainer>
   );
 };
 
