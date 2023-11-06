61f28563d39e8462ba49ba860cf270ab96096e8d
Artem Kabanov <artemkabanov108@gmail.com>
2023-11-06 18:26:48 +0200

    delete: deleted smorer block from key contacts block cannabis page

--- requests/practices/practicesQueryGenerator.js
+++ requests/practices/practicesQueryGenerator.js
@@ -25,10 +25,6 @@ export const practicesQueryGenerator = (uri) => {
 						listOfLegalGuidanceRegarding {
 							issue
 						}
-						underlay {
-							altText
-							sourceUrl
-						}
 					}
 					helpArticleBlock {
 						title
