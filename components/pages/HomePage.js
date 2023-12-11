52b7d635093788f513cffefd78768f583d9fd424
Artem Kabanov <artemkabanov108@gmail.com>
2023-12-11 11:26:32 +0200

    feat: new aboutFirm box

--- components/pages/HomePage.js
+++ components/pages/HomePage.js
@@ -2,7 +2,6 @@ import dynamic from 'next/dynamic';
 import HomeBanner from 'components/organisms/home/HomeBanner';
 import HomeSiteHead from 'components/shared/head/HomeSiteHead';
 import { CURRENT_DOMAIN } from 'utils/constants';
-import { AboutContainer } from 'styles/AboutFirm.style';
 import HappyHolidayLink from 'components/molecules/home/HappyHolidayLink';
 // import InfoModal from '../atoms/InfoModal';
 
@@ -36,24 +35,48 @@ const HomePage = ({
   isHoliday,
   offices,
   firmNewsArticles,
-}) => (
-  <>
-    <HomeSiteHead
-      title={seo.title}
-      metaDescription={seo.metaDesc}
-      canonicalUrl={CURRENT_DOMAIN}
-    />
-    <HomeBanner {...banner} />
-    {isHoliday && <HappyHolidayLink />}
-    {/* <InfoModal /> */}
-    <AboutContainer>
-      <AboutFirmSection {...aboutFirm} />
-      <AboutFirmSection {...aboutFirm2} />
-    </AboutContainer>
-    <AllOfficeLocations offices={offices} />
-    <FirmNews firmNews={firmNewsArticles} />
-    <Awards awards={awards} />
-  </>
-);
+}) => {
+  const aboutFirmProps = {
+    infos: [
+      {
+        description: aboutFirm.description,
+        title: aboutFirm.title,
+        subTitle: aboutFirm.subTitle,
+      },
+      {
+        description: aboutFirm2.description,
+        title: aboutFirm2.title,
+        subTitle: aboutFirm2.subTitle,
+      },
+    ],
+    linksBtn: [
+      {
+        linkLabel: aboutFirm.linkLabel,
+        linkUrl: aboutFirm.linkUrl,
+      },
+      {
+        linkLabel: aboutFirm2.linkLabel,
+        linkUrl: aboutFirm2.linkUrl,
+      },
+    ],
+  };
+
+  return (
+    <>
+      <HomeSiteHead
+        title={seo.title}
+        metaDescription={seo.metaDesc}
+        canonicalUrl={CURRENT_DOMAIN}
+      />
+      <HomeBanner {...banner} />
+      {isHoliday && <HappyHolidayLink />}
+      {/* <InfoModal /> */}
+      <AboutFirmSection {...aboutFirmProps} />
+      <AllOfficeLocations offices={offices} />
+      <FirmNews firmNews={firmNewsArticles} />
+      <Awards awards={awards} />
+    </>
+  );
+};
 
 export default HomePage;
