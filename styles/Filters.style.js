9d986443ca4a57b2b8244894cbac8408d3f38f8f
Artem Kabanov <artemkabanov108@gmail.com>
2024-02-09 14:50:37 +0200

    fix: search filter fixrd on tablet screen v2

--- styles/Filters.style.js
+++ styles/Filters.style.js
@@ -96,11 +96,6 @@ export const DropdownSelectorBtn = styled(DropdownButton)`
           : null}
     }
   }
-
-  ${media_breakpoint_down('md')} {
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-  }
 `;
 
 export const ContainerFilters = styled(Container)`
@@ -124,8 +119,18 @@ export const ContainerFilters = styled(Container)`
   }
 
   ${media_breakpoint_down('md')} {
-    form {
-      width: 100%;
+    .row {
+      > :nth-child(-n + 2) {
+        margin-bottom: 0;
+      }
+
+      > :nth-child(3) {
+        margin-bottom: 10px;
+      }
+
+      form {
+        width: 100%;
+      }
     }
   }
 `;
