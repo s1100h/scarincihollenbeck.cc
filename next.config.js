317d071fd0b05dc12adf22e34e7d0dd8cf237892
Artem Kabanov <artemkabanov108@gmail.com>
2024-03-04 16:15:28 +0200

    test: build test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 const {
   SITE_PAGES_REWRITES,
   POST_TYPE_REWRITES,
@@ -5,7 +15,90 @@ const {
 } = require('./utils/rewrites');
 const { PERMANENT_REDIRECTS } = require('./utils/permanent-redirects');
 
-module.exports = {
+// module.exports = {
+//   env: {
+//     NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
+//     NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
+//     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+//     NEXT_PUBLIC_ALGOLIA_PUBLIC_API: process.env.NEXT_PUBLIC_ALGOLIA_PUBLIC_API,
+//     NEXT_PUBLIC_ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
+//     NEXT_PUBLIC_ALGOLIA_SEARCH_INDEX:
+//       process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_INDEX,
+//     NEXT_PUBLIC_BASE_GRAPHQL_API_URL:
+//       process.env.NEXT_PUBLIC_BASE_GRAPHQL_API_URL,
+//   },
+//   images: {
+//     domains: [
+//       'shhcsgmvsndmxmpq.nyc3.digitaloceanspaces.com',
+//       'musicesq.com',
+//       'scarincilawyer.com',
+//       'conqqqshlskopvv.nyc3.digitaloceanspaces.com',
+//       'wp.scarincihollenbeck.com',
+//       'res.cloudinary.com',
+//     ],
+//     minimumCacheTTL: 60,
+//     deviceSizes: [576, 768, 992, 1200, 1456],
+//     // staticPageGenerationTimeout: 120,
+//     // swcMinify: true,
+//   },
+//   compiler: {
+//     styledComponents: true,
+//   },
+//   experimental: {
+//     fontLoaders: [
+//       { loader: 'next/font/google', options: { subsets: ['latin'] } },
+//     ],
+//   },
+// };
+
+const securityHeaders = [
+  {
+    key: 'Cache-Control',
+    value: 's-maxage=8600, stale-while-revalidate',
+  },
+];
+
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shhcsgmvsndmxmpq.nyc3.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'musicesq.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scarincilawyer.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'conqqqshlskopvv.nyc3.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wp.scarincihollenbeck.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+    minimumCacheTTL: 60,
+    deviceSizes: [576, 768, 992, 1200, 1456],
+  },
+  experimental: {
+    scrollRestoration: true,
+    nextScriptWorkers: true,
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   env: {
     NEXT_PUBLIC_GA_TRACKING_ID: process.env.NEXT_PUBLIC_GA_TRACKING_ID,
     NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
@@ -17,28 +110,21 @@ module.exports = {
     NEXT_PUBLIC_BASE_GRAPHQL_API_URL:
       process.env.NEXT_PUBLIC_BASE_GRAPHQL_API_URL,
   },
-  images: {
-    domains: [
-      'shhcsgmvsndmxmpq.nyc3.digitaloceanspaces.com',
-      'musicesq.com',
-      'scarincilawyer.com',
-      'conqqqshlskopvv.nyc3.digitaloceanspaces.com',
-      'wp.scarincihollenbeck.com',
-      'res.cloudinary.com',
-    ],
-    minimumCacheTTL: 60,
-    deviceSizes: [576, 768, 992, 1200, 1456],
-    // staticPageGenerationTimeout: 120,
-    // swcMinify: true,
-  },
   compiler: {
     styledComponents: true,
   },
-  // experimental: {
-  //   fontLoaders: [
-  //     { loader: 'next/font/google', options: { subsets: ['latin'] } },
-  //   ],
-  // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+
+  async redirects() {
+    return [...PERMANENT_REDIRECTS];
+  },
   async rewrites() {
     return [
       ...POST_TYPE_REWRITES,
@@ -46,7 +132,7 @@ module.exports = {
       ...SITE_PAGES_REWRITES,
     ];
   },
-  async redirects() {
-    return [...PERMANENT_REDIRECTS];
-  },
 };
+
+// @ts-ignore
+module.exports = withPWA(nextConfig);
