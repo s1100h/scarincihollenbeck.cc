43f72a72e1492ca14de674d1fd1f8c09ab88a2a4
Artem Kabanov <artemkabanov108@gmail.com>
2024-04-12 22:06:53 +0300

    feat: new link to methodology

--- components/atoms/ContentTitle.js
+++ components/atoms/ContentTitle.js
@@ -1,4 +1,9 @@
 import { BigGrayTitle } from 'styles/BigGrayTitle.style';
 
-const ContentTitle = ({ title }) => <BigGrayTitle>{title}</BigGrayTitle>;
+const ContentTitle = ({ title, children }) => (
+  <BigGrayTitle>
+    {title}
+    {children}
+  </BigGrayTitle>
+);
 export default ContentTitle;
