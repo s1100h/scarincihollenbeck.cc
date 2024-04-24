546feecf77ad048e0793ea4e6e153c7a1e817d08
Danila <lapshov.d@mettevo.org>
2024-04-24 12:15:22 +0300

    FIX: change anchor top bar on sticky

--- styles/practices/PracticeAnchors.style.js
+++ styles/practices/PracticeAnchors.style.js
@@ -1,61 +1,30 @@
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 import {
   media_breakpoint_down,
   media_breakpoint_exactly_down,
 } from 'styles/mediaBreakpoints.style';
 
-const fade = keyframes`
-  from {
-    opacity: 0;
-    transform: translateY(-20px);
-  }
-  to {
-    opacity: 1;
-    transform: translateY(0);
-  }
-`;
-
 export const PracticeAnchorsHolder = styled.div`
   margin-top: 40px;
-  position: relative;
+  position: sticky;
+  top: 155px;
+  left: 0;
   transition: all 0.3s ease;
+  z-index: 2;
 
-  &.fixed {
-    width: 100%;
-    position: fixed;
+  ${media_breakpoint_exactly_down(1439)} {
+    margin-top: 32px;
+  }
+
+  ${media_breakpoint_down('lg')} {
     top: 115px;
-    left: 0;
-    z-index: 10;
-    opacity: 1;
-
-    ${media_breakpoint_exactly_down(1439)} {
-      top: 125px;
-    }
-
-    ${media_breakpoint_down('lg')} {
-      top: 100px;
-    }
-
-    ${media_breakpoint_down('md')} {
-      top: 48px;
-    }
-
-    ${media_breakpoint_exactly_down(640)} {
-      top: 98px;
-    }
   }
 
-  ${media_breakpoint_exactly_down(1439)} {
-    margin-top: 32px;
+  ${media_breakpoint_down('md')} {
+    top: 75px;
   }
-`;
 
-export const PracticeAnchorEmpty = styled.div`
-  display: ${({ show }) => (show ? 'block' : 'none')};
-  margin-top: 40px;
-  height: ${({ height }) => `${height}px`};
-
-  ${media_breakpoint_exactly_down(1439)} {
-    margin-top: 32px;
+  ${media_breakpoint_exactly_down(640)} {
+    top: 125px;
   }
 `;
