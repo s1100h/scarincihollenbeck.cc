ed90bc95ba3bc7f4c10f1bc47bef7e2bb02e94b2
Danila <lapshov.d@mettevo.org>
2024-09-04 15:18:55 +0300

    FIX: gitignore add snapshots

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { RouterContext } from 'next/dist/shared/lib/router-context.shared-runtime';
 import mockRouter from 'next-router-mock';
 import renderer from 'react-test-renderer';
+import HomeContactForm from 'components/organisms/home/HomeContactForm';
 import HomePage from '../components/pages/HomePage';
 import { wpGraphQl } from '../redux/services/wp-graphql';
 import { appApi } from '../redux/services/project-api';
@@ -14,11 +15,14 @@ import sizesReducer from '../redux/slices/sizes.slice';
 import AboutFirm from '../components/organisms/home/AboutFirm';
 
 // Mocking next/dynamic
-jest.mock('next/dynamic', () => (component) => function DynamicComponent() {
-  return (
-    <section data-testid={component.displayName || 'dynamic-component'} />
-  );
-});
+jest.mock(
+  'next/dynamic',
+  () => (component) => function DynamicComponent() {
+    return (
+      <section data-testid={component.displayName || 'dynamic-component'} />
+    );
+  },
+);
 
 // Set up the store
 const store = configureStore({
@@ -73,10 +77,27 @@ describe('HomePage', () => {
         heroProfileLink={{ title: 'link', url: '/' }}
         aboutHero="Super hero - super skill"
         arcticle="Frog"
-        heroPhoto="/images/no-image-found-diamond.png"
+        heroPhoto={{
+          sourceUrl: '/images/no-image-found-diamond.png',
+          altText: 'logo diamond',
+        }}
       />,
     );
     const whoWeAreSection = screen.getByTestId('who-we-are');
     expect(whoWeAreSection).toBeInTheDocument();
+  });
+
+  it('The form home page render', () => {
+    const component = renderer.create(
+      <Provider store={store}>
+        <RouterContext.Provider value={mockRouter}>
+          <HomeContactForm />
+        </RouterContext.Provider>
+      </Provider>,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // const formWrapper = screen.getByTestId('ContactFormWrapper');
   });
 });
